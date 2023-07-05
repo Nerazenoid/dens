20bceb4d8a8cb8aa974498433290bb7c558a6fa3
Nera <omega3050h@mail.ru>
2023-07-05 16:41:52 +0300

    fiiiive

--- dens/src/App.js
+++ dens/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import TopBar from './components/header/TopBar.jsx';
 import MainSlider from './components/MainSlider';
+import Advantages from './components/advantages/Advantages';
+import Services from './components/services/Services';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
         <TopBar />
         <div className='main_content'>
           <MainSlider />
+          <Advantages />
+          <Services />
         </div>
     </div>
   );
